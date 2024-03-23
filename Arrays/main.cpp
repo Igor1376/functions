03ee91ada6a3d1b99d57f69426fad4454076629d
Igor <bignick13@yandex.ru>
2024-03-23 16:38:52 +0300

    Function work home

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,6 +6,9 @@ void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void Print(const int arr[], const int n);
 int Sum(const int arr[], const int n);
 double Avg(const int arr[], const int n);
+int minValueIn(const int arr[], const int n);
+int maxValueIn(const int arr[], const int n);
+void shiftLeft(const int arr[], const int n);
 
 void main()
 {
@@ -24,6 +27,9 @@ void main()
 	Print(arr, n);
 	cout << "Сумма элементов массива: " << Sum(arr, n) << endl;
 	cout << "Среднее арифметическое элементов массива: " << Avg(arr, n) << endl;
+	cout << "Минимальное значение массива: " << minValueIn(arr, n) << endl;
+	cout << "Максимальное значение массива: " << maxValueIn(arr, n) << endl;
+	shiftLeft(arr, n);
 	
 }
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -58,4 +64,78 @@ int Sum(const int arr[], const int n)
 double Avg(const int arr[], const int n)
 {
 	return (double)Sum(arr, n) / n;
+}
+int minValueIn(const int arr[], const int n)
+{
+	int minValue;
+	minValue = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < minValue)minValue = arr[i];
+	}
+	return minValue;
+}
+int maxValueIn(const int arr[], const int n)
+{
+	int maxValue;
+	maxValue = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > maxValue)maxValue = arr[i];
+	}
+	return maxValue;
+}
+
+//const int n = 10;
+//int arr[n] = { 0,1,1,2,3,5,8,13,21,34 };
+//for (int i = 0; i < n; i++)
+//{
+//	cout << arr[i] << "\t";
+//}
+//cout << endl;
+//
+//int number_of_shift;
+//cout << "На сколько элементов сдвинуть массив: "; cin >> number_of_shift;
+//for (int i = 0; i < number_of_shift; i++)
+//{
+//	int buffer = arr[0];
+//	for (int i = 0; i < n - 1; i++)
+//	{
+//		arr[i] = arr[i + 1];
+//	}
+//	arr[n - 1] = buffer;
+//	system("CLS"); // функция system () вызывает любую команду Windows
+//	// команда CLS clear screen очищает окно консоли
+//
+//	for (int i = 0; i < n; i++)
+//	{
+//		cout << arr[i] << "\t";
+//	}
+//	cout << endl;
+//	Sleep(1000); // функция sleep () приостанавливает выполнение программы на заданный промежуток времени
+//	// промежуток времени задается в миллисекундах.
+//}
+void shiftLeft(const int arr[], const int n)
+{
+	
+	int number_of_shift;
+	cout << "На сколько элементов сдвинуть массив: "; cin >> number_of_shift;
+	for (int i = 0; i < number_of_shift; i++)
+	{
+		const int n = 5;
+		int arr[n];
+		int buffer = arr[0];
+		for (int i = 0; i < n - 1; i++)
+		{
+			arr[i] = arr[i + 1];
+		}
+		arr[n - 1] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+		cout << arr[i] << "\t";
+		}
+		
+	}
+	
+	
 }
