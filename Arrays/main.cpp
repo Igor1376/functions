9046ca9bc43eda8c129bc1aefe0ecbe6598e682a
Igor <bignick13@yandex.ru>
2024-04-18 18:43:58 +0300

    SeparatedTemplates

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -3,6 +3,7 @@ using namespace std;
 #define tab "\t"
 
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
+
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
 
 template<typename T>
