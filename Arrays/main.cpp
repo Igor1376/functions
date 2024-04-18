8838248b10004148688d299c46d9606d66b499c0
Igor <bignick13@yandex.ru>
2024-04-18 18:54:15 +0300

    Codding

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -35,35 +35,7 @@ void main()
 		
 }
 
-void FillRand(int arr[], const int n, int minRand, int maxRand)
-{
-	if (maxRand < minRand)
-	{
-		int buffer = minRand;
-		minRand = maxRand;
-		maxRand = buffer;
-	}
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = minRand + rand() % (maxRand - minRand);
-	}
-}
-void FillRand(double arr[], const int n, int minRand, int maxRand)
-{
-	if (maxRand < minRand)
-	{
-		int buffer = minRand;
-		minRand = maxRand;
-		maxRand = buffer;
-	}
-	minRand *= 100;
-	maxRand *= 100;
-	for (int i = 0; i < n; i++)
-	{
-		arr[i] = minRand + rand() % (maxRand - minRand);
-		arr[i] /= 100;
-	}
-}
+
 template<typename T>
 void Print(const T arr[], const int n)
 {
